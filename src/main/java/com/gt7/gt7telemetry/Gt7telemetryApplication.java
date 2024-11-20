882a86f0a7b1f791e1c0a9b6111179cd4fb056d3
jckgomescruz <jckgomescruz@gmail.com>
2024-11-20 13:53:43 -0300

    Initial Commit

---
+++ src/main/java/com/gt7/gt7telemetry/Gt7telemetryApplication.java
@@ -0,0 +1,13 @@
+package com.gt7.gt7telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gt7telemetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gt7telemetryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gt7/gt7telemetry/Gt7telemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gt7.gt7telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gt7telemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
